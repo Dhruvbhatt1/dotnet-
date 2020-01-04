da8f2a89bba4b8455f9320f222f31d46095940d1
DHRUV BHATT <17comp.deeep.fuletra@gmail.com>
2020-01-04 11:26:54 +0530

    Initial commit

---
+++ Dotnetsolutions/Practical1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practical1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Please include this line into to your code
+            Console.WriteLine("Hello world");
+            Console.Read();
+        }
+    }
+}
